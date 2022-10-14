a900730359d4b25fed9e2c293cbd9c403011777b
caowens <caowens555@yahoo.com>
2022-10-14 18:13:50 -0500

    Add boilerplate

--- backend/server.js
+++ backend/server.js
@@ -4,3 +4,71 @@ const express = require('express');
 const app = express();
 
 app.listen(3000);
+
+
+/** Reference code: https://github.com/bpeddapudi/nodejs-basics-routes/blob/master/server.js 
+ * import express */
+
+ let carsMockData = [
+    {
+        "id": 1,
+        "brand": "Hyundai",
+        "name": "Ioniq",
+        "releaseYear": 2017,
+        "color": "blue"
+    },
+    {
+        "id": 2,
+        "brand": "Toyota",
+        "name": "Prius",
+        "releaseYear": 2007,
+        "color": "blue"
+    },
+    {
+        "id": 3,
+        "brand": "Chevrolet",
+        "name": "Aveo",
+        "releaseYear": 2007,
+        "color": "white"
+    },
+    {
+        "id": 4,
+        "brand": "BMW",
+        "name": "M5",
+        "releaseYear": 2017,
+        "color": "White"
+    },
+    {
+        "id": 5,
+        "brand": "Tesla",
+        "name": "S",
+        "releaseYear": 2019,
+        "color": "Black"
+    }
+]
+
+/** Create GET API. API shoudl return  const carsMockData*/
+
+
+
+
+
+/** Create POST API. Get the new car data from react. 
+ *      Check if car with id exists. If Yes return 500. With message 'Car already exists'
+ *      If there is no car with the id, add the new car to  carsMockData and return carsMockData as response */
+
+
+
+
+
+/** Create PUT API. 
+ *  Check if car with id exists. If No return 500 with error 'No car with given id exist'. 
+ *  If there is car with the requested id, update that car's data in 'carsMockData' and return 'carsMockData' */
+
+
+
+
+/** Create Delete API. 
+ *  Check if car with id exists. If No return 500. With message 'No car with give id exists'
+ *  If there is car with the requested id. Delete that car from 'carsMockData' and return 'carsMockData'
+*/
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,142 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function Cars() {
+/**
+ * Fields required for the car
+      "id",
+      "brand",
+      "name",
+      "releaseYear",
+      "color"
+ */
+  const carFormInitialData = {
+    id: 0,
+    name: ''
+  }
+  const [carFormData, setCarFormData] = useState(carFormInitialData);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setCarFormData({
+      ...carFormData,
+      [name]: value,
+    });
+  }
+
+  const handleSubmit = (event) => {
+    /**
+     * Gather all the form data to state variable carFormData
+     * When the form is submitted POST the data to Backend using fetch post
+     * https://googlechrome.github.io/samples/fetch-api/fetch-post.html
+     */
+    event.preventDefault();
+  }
+
+  const handleDelete = () => {
+    /**
+     * When clicked on a delete button, get the id of the car's delete button clicked
+     * Then use javascript fetch to send DELETE request to NodeJS
+     * https://openjavascript.info/2022/01/03/using-fetch-to-make-get-post-put-and-delete-requests/
+     */
+  }
+
+/** 🥳🥳🥳🥳🥳🥳 DOUBLE BONUS POINTS 🥳🥳🥳🥳🥳🥳 */
+  const handleEdit = () => {
+    /**
+     * When clicked on a edit button figure out a way to edit the car data.
+     * Once edited send the updated data to NodeJS.
+     * Then use javascript fetch to send DELETE request to NodeJS
+     * https://openjavascript.info/2022/01/03/using-fetch-to-make-get-post-put-and-delete-requests/
+     */
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='cars-from-wrapper'>
+      <form id="cars-form" onSubmit={handleSubmit} autoComplete="off">
+        {/** 
+           * TODO: Update the form fields with inputs for 
+           *    ID, Brand, Name, ReleaseYear and Color
+           * Make required changes to  const carFormInitialData
+           * */}  
+        <label>
+          ID:
+          <input name='id' type="text" value={carFormData.id} onChange={handleInputChange} />
+        </label>
+        <label>
+          Name:
+          <input name='name' type="text" value={carFormData.name} onChange={handleInputChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+       {/** 
+           * TODO: Update the code below to see any new proprties added to carFormData
+           * */}  
+      <p>ID:{carFormData.id}, name:{carFormData.name}</p>
+
+      <h2>Cars Data</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Car Make</th>
+            <th>Car Model</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/** 
+           * TODO: Replace this code with Data from Node JS GET api data
+           * React documentation: https://reactjs.org/docs/lists-and-keys.html
+           * How to get data from API: https://www.w3schools.com/jsref/api_fetch.asp
+           * */}          
+          <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+            <td>✎</td>
+            <td>🗑</td>        
+          </tr>
+          <tr>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+            <td>✎</td>
+            <td>🗑</td>
+          </tr>
+          <tr>
+            <td>Ernst Handel</td>
+            <td>Roland Mendel</td>
+            <td>Austria</td>
+            <td>✎</td>
+            <td>🗑</td>
+          </tr>
+          <tr>
+            <td>Island Trading</td>
+            <td>Helen Bennett</td>
+            <td>UK</td>
+            <td>✎</td>
+            <td>🗑</td>
+          </tr>
+          <tr>
+            <td>Laughing Bacchus Winecellars</td>
+            <td>Yoshi Tannamuri</td>
+            <td>Canada</td>
+            <td>✎</td>
+            <td>🗑</td>
+          </tr>
+          <tr>
+            <td>Magazzini Alimentari Riuniti</td>
+            <td>Giovanni Rovelli</td>
+            <td>Italy</td>
+            <td>✎</td>
+            <td>🗑</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
 
-export default App;
+export default Cars;
