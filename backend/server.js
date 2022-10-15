cf1bc8bb5bd1f2b3744088fbe12c476c33d59e78
caowens <caowens555@yahoo.com>
2022-10-14 22:17:07 -0500

    Implement backend requests

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,25 @@
-console.log('something');
-
-const express = require('express');
-const app = express();
-
-app.listen(3000);
-
-
 /** Reference code: https://github.com/bpeddapudi/nodejs-basics-routes/blob/master/server.js 
  * import express */
 
- let carsMockData = [
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+
+// To support cors. 
+app.use(cors())
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+ app.use(bodyParser.json());
+
+let carsMockData = [
     {
         "id": 1,
         "brand": "Hyundai",
@@ -48,7 +58,9 @@ app.listen(3000);
 ]
 
 /** Create GET API. API shoudl return  const carsMockData*/
-
+app.get("/cars", (req, res) => {
+    res.send(carsMockData);
+}); 
 
 
 
@@ -56,7 +68,24 @@ app.listen(3000);
 /** Create POST API. Get the new car data from react. 
  *      Check if car with id exists. If Yes return 500. With message 'Car already exists'
  *      If there is no car with the id, add the new car to  carsMockData and return carsMockData as response */
+app.post("/save", (req, res) => {
+    let id = req.body.id;
+    let brand = req.body.brand;
+    let name = req.body.name;
+    let releaseYear = req.body.releaseYear;
+    let color = req.body.color;
 
+    carsMockData.forEach((car) => {
+        if (car.id === id) {
+            res.sendStatus(500).send('Car already exists');
+            return;
+        }
+    })
+
+    carsMockData.push({"id": id, "brand": brand, "name": name, "releaseYear": releaseYear, "color": color});
+    console.log(carsMockData);
+    res.sendStatus(200);
+})
 
 
 
@@ -64,7 +93,27 @@ app.listen(3000);
 /** Create PUT API. 
  *  Check if car with id exists. If No return 500 with error 'No car with given id exist'. 
  *  If there is car with the requested id, update that car's data in 'carsMockData' and return 'carsMockData' */
+app.put("/edit", (req,res) => {
+    let id = req.body.id;
+    let brand = req.body.brand;
+    let name = req.body.name;
+    let releaseYear = req.body.releaseYear;
+    let color = req.body.color;
 
+    carsMockData.forEach((car) => {
+        if (car.id === id) {
+            car.brand = brand;
+            car.name = name;
+            car.releaseYear = releaseYear;
+            car.color = color;
+            console.log(carsMockData);
+            res.sendStatus(200);
+        }
+    })
+
+    console.log('No car with given id exist');
+    res.sendStatus(500);
+})
 
 
 
@@ -72,3 +121,22 @@ app.listen(3000);
  *  Check if car with id exists. If No return 500. With message 'No car with give id exists'
  *  If there is car with the requested id. Delete that car from 'carsMockData' and return 'carsMockData'
 */
+app.delete("/delete", (req, res) => {
+    let id = req.body.id;
+    deleted = False;
+    
+    carsMockData.forEach((car) => {
+        if (car.id === id) {
+            let index = carsMockData.indexOf(car);
+            carsMockData.splice(index, 1);
+            console.log(carsMockData);
+            deleted = True;
+            res.sendStatus(200);
+        }
+    });
+
+    console.log('No car with given id exists');
+    res.sendStatus(500);
+})
+
+app.listen(8000);
